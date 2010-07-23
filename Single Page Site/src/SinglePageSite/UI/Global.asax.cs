81f6218db5b153f064fe87f8223c6e3b0bbad867
Tim G. Thomas <tim@timgthomas.com>
2010-07-23 09:24:05 -0500

    Adding empty MVC application structure.

---
+++ Single Page Site/src/SinglePageSite/UI/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UI
+{
+	public class MvcApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		private static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute("Default", "{controller}/{action}/{id}",
+				 new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+		}
+	}
+}
